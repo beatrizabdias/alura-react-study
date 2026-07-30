9e26b5003a0212abd39f4635581e1e40e669d229
Beatriz <beatrizcabdias@gmail.com>
2026-07-30 11:09:32 -0300

    feat: add header, banner and form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import './App.css'
 
+function FormularioDeEvento(){
+  return (
+    <form className="form-evento">
+      <h2>Preencha para criar um evento</h2>
+      <fieldset>
+        <label htmlFor="nome">Qual o nome do evento?</label>
+        <input type="text" id="nome" />
+      </fieldset>
+    </form>
+
+  )
+}
+
 function App() {
   return (
-    <div>
-      <h1>React Study Project</h1>
-      <p>Project from Alura course</p>
-    </div>
+    <main>
+      <header>
+        <img src="/logo.png" alt="" />
+      </header>
+      <section>
+        <img src="/banner.png" alt="" />
+      </section>
+      <FormularioDeEvento/>
+    </main>
   )
 }
 
