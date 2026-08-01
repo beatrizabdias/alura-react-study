14316f83c93a6d04976db45bcff3c143536da4c8
Beatriz <beatrizcabdias@gmail.com>
2026-08-01 18:54:10 -0300

    feat: add event filtering by theme and container layout

--- src/App.jsx
+++ src/App.jsx
@@ -55,16 +55,29 @@ function App() {
       </header>
       <Banner/>
       <FormularioDeEvento temas={temas} aoSubmeter={adicionarEvento}/>
-      {temas.map(function(item){
-        return(
-          <section key={item.id}>
-            <Tema tema={item}/>
-            {eventos.map(function (item, index){
-              return <CardEvento evento={item} key={index}/>
-            })}
-          </section>
-        )
-      })}
+      <section className="container">
+        {temas.map(function(tema){
+          if (!eventos.some(function (evento){
+            return evento.tema.id == tema.id
+  
+          })) {
+            return null
+            
+          }
+          return(
+            <section key={tema.id}>
+              <Tema tema={tema}/>
+              <div className="eventos">
+              {eventos.filter(function (evento){
+                return evento.tema.id == tema.id
+              }).map(function (evento, index){
+                return <CardEvento evento={evento} key={index}/>
+              })}
+              </div>
+            </section>
+          )
+        })}
+      </section>
     </main>
   )
 }
